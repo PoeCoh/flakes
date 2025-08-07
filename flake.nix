db4f0a925ee7584189f49a33a7f9087714227fe3
Ben Jordan <ben@poecoh.com>
2025-08-07 17:57:59 -0400

    asdf

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils = "github:numtide/flake-utils";
+    flake-utils.url = "github:numtide/flake-utils";
 
     # flakes
     zig-flake.url = "git+http://git.poecoh.com/flakes/zig.git";
