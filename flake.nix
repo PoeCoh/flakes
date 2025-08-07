d0e8887e3c2eb443a329b7d9cb83ccf97bf735f0
Ben Jordan <ben@poecoh.com>
2025-08-07 17:56:14 -0400

    hm

--- flake.nix
+++ flake.nix
@@ -3,19 +3,23 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils = "github:numtide/flake-utils";
+
+    # flakes
     zig-flake.url = "git+http://git.poecoh.com/flakes/zig.git";
   };
 
   outputs = {
     self,
     nixpkgs,
+    flake-utils,
     zig-flake
-  }: {
+  }: flake-utils.lib.eachDefaultSystem (system: {
     devShells.x86_64-linux = {
       zig = zig-flake.devShells.x86_64-linux.default;
     };
     packages.x86_64-linux = {
       zig = zig-flake.packages.x86_64-linux.default;
     };
-  };
+  });
 }
